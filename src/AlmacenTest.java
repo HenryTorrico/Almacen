a4bf1a1ce01d731622347e36eb5736909770bbc1
HenryTorrico <guillerbauti@gmail.com>
2020-02-26 17:34:44 -0400

    test for calculate rate of service

--- src/AlmacenTest.java
+++ src/AlmacenTest.java
@@ -5,10 +5,14 @@ import junit.framework.Assert;
 public class AlmacenTest {
 	
 	@Test
-	public void testForCreateProduct() {
+	public void testForAvailableService() {
 		Producto prod1 = new Servicio("Lavanderia",5,25); 
 		Assert.assertEquals(true, prod1.estaDisponible(prod1.getCant())); 
 	}
 	
-	
+	@Test
+	public void testForCalculateServiceRate() {
+		Producto prod1 = new Servicio("Lavanderia",5,25); 
+		Assert.assertEquals(125, prod1.calcularTarifa(prod1.getCant())); 
+	}
 }
